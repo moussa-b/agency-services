3b16266ba9705d73c872a2a7fe546841b553dee0
moussa <moussa@bdmz.fr>
2025-01-03 18:09:47 +0100

    add swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform/transform.interceptor';
 import { ConfigService } from "@nestjs/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalInterceptors(new TransformInterceptor()); //clean null values in JSON
 
+  // CORS config
   const configService = app.get(ConfigService);
   const corsOrigins = configService
     .get<string>('CORS_ORIGINS', '')
@@ -17,6 +20,27 @@ async function bootstrap() {
     origin: corsOrigins,
     credentials: true,
   });
+
+  // Swagger
+  // Warning SWAGGER_ROUTE must start with /
+  if (configService.get<string>('SWAGGER_ROUTE', '') && configService.get<string>('SWAGGER_USER', '') && configService.get<string>('SWAGGER_PASSWORD', '')) {
+    app.use( // Basic Authentication middleware to the Swagger route
+      [configService.get<string>('SWAGGER_ROUTE', ''), `${configService.get<string>('SWAGGER_ROUTE', '')}-json`],
+      basicAuth({
+        users: {
+          [configService.get<string>('SWAGGER_USER', '')]: configService.get<string>('SWAGGER_PASSWORD', ''),
+        },
+        challenge: true, // enables the browser prompt
+      }),
+    );
+    const config = new DocumentBuilder()
+      .setTitle('Clients')
+      .setDescription('The clients API description')
+      .setVersion(require('../package.json').version)
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(configService.get<string>('SWAGGER_ROUTE', ''), app, documentFactory);
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
