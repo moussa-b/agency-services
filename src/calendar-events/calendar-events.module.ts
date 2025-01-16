5527d6f8a3aa0710a96b4889aae729286bdcf200
moussa <moussa@bdmz.fr>
2025-01-16 10:27:48 +0100

    fix health indicator

--- src/calendar-events/calendar-events.module.ts
+++ src/calendar-events/calendar-events.module.ts
@@ -3,9 +3,11 @@ import { CalendarEventsService } from './calendar-events.service';
 import { CalendarEventsController } from './calendar-events.controller';
 import { ConfigService } from "@nestjs/config";
 import { CalendarEventsRepository } from "./calendar-events.repository";
+import { CalendarEventsHealthIndicator } from "./calendar-events-health.indicator";
 
 @Module({
   controllers: [CalendarEventsController],
-  providers: [ConfigService, CalendarEventsRepository, CalendarEventsService],
+  providers: [ConfigService, CalendarEventsRepository, CalendarEventsService, CalendarEventsHealthIndicator],
+  exports: [CalendarEventsHealthIndicator]
 })
 export class CalendarEventsModule {}
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -4,9 +4,10 @@ import { TerminusModule } from '@nestjs/terminus';
 import { SharedModule } from '../shared/shared.module';
 import { UsersModule } from '../users/users.module';
 import { ClientsModule } from '../clients/clients.module';
+import { CalendarEventsModule } from "../calendar-events/calendar-events.module";
 
 @Module({
-  imports: [TerminusModule, SharedModule, UsersModule, ClientsModule],
+  imports: [TerminusModule, SharedModule, UsersModule, ClientsModule, CalendarEventsModule],
   controllers: [HealthController],
 })
 export class HealthModule {}
